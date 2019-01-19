ca5e590e3fb5ed0a9f208766129a02bdfcdc1741
maxiaoyu <maxiaoyu@limikeji.com>
2019-01-19 16:44:55 +0800

    first commit

---
+++ my-project/src/components/SayHi.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='hi'>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return { 
+        message: "hello maxiaoyu!"
+    }
+  }
+}
+</script>
+
+<style>
+.hi {
+    color: red;
+    font-size: 22px;
+}
+</style>
